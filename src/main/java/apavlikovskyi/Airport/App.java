72977c183680961fdfb5f976ee3baa6213ae2e9f
Artemchik <apavlikovskyi@gmail.com>
2017-03-31 12:31:02 +0300

    sql+flyway

--- src/main/java/apavlikovskyi/Airport/App.java
+++ src/main/java/apavlikovskyi/Airport/App.java
@@ -1,9 +1,19 @@
 package apavlikovskyi.Airport;
 
+import org.flywaydb.core.Flyway;
+
 /**
  * Created by Diana P on 17.03.2017.
  */
 
 public class App {
+   static final private String url="jdbc:mysql://127.0.0.1:3307/airport";
+   static final private String user="airport";
+   static final private String password="admin";
 
+    public static void main(String[] args) {
+        Flyway flyway = new Flyway();
+        flyway.setDataSource(url, user, password);
+        flyway.migrate();
+    }
 }
