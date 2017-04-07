eb72efc88bb83f453b4448bf56d4eb1b355fe5ad
Artemchik <apavlikovskyi@gmail.com>
2017-04-07 15:44:57 +0300

    dao&class_entity

---
+++ src/test/java/apavlikovskyi/Airport/DAO/TicketDAOTest.java
@@ -0,0 +1,51 @@
+package apavlikovskyi.Airport.DAO;
+
+import apavlikovskyi.Airport.DAO.DAOUtil.DataBaseConnection;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Diana P on 06.04.2017.
+ */
+public class TicketDAOTest {
+
+    @Before
+    public void before(){
+
+    }
+
+    @After
+    public void after(){
+
+    }
+
+
+    @Test
+    public void delete() throws Exception {
+        DataBaseConnection.migrate();
+    }
+
+    @Test
+    public void getAll() throws Exception {
+
+    }
+
+    @Test
+    public void getById() throws Exception {
+
+    }
+
+    @Test
+    public void insert() throws Exception {
+
+    }
+
+    @Test
+    public void update() throws Exception {
+
+    }
+
+}
